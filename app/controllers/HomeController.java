23128b371184d07839e3cd5fa3b353fb722ff316
Stefan Huber <stefan.huber@beyondit.at>
2018-03-27 17:28:25 +0200

    feat: create

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,33 +1,42 @@
 package controllers;
 
 import models.Note;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.*;
+import services.InMemoryNoteRepository;
 
-import java.util.ArrayList;
+import javax.inject.Inject;
 import java.util.List;
 
 public class HomeController extends Controller {
 
+    protected InMemoryNoteRepository noteRepository;
+
+    protected Form<Note> noteForm;
+
+
+
+    @Inject
+    public HomeController(InMemoryNoteRepository noteRepository, FormFactory formFactory) {
+        this.noteRepository = noteRepository;
+        this.noteForm = formFactory.form(Note.class);
+    }
+
     public Result index() {
-        List<Note> notes = new ArrayList<>();
-
-        Note note1 = new Note();
-        note1.setId(1);
-        note1.setTitle("erste Notiz");
-        note1.setLastEdited(1521364620);
-        notes.add(note1);
-
-        Note note2 = new Note();
-        note2.setId(2);
-        note2.setTitle("zweite Notiz");
-        note2.setLastEdited(1523434663);
-        notes.add(note2);
-
+        List<Note> notes = noteRepository.getNotes();
         return ok(views.html.index.render(notes));
     }
 
     public Result form(int id) {
         return ok(views.html.form.render());
+    }
+
+    public Result save() {
+        Note newNote = noteForm.bindFromRequest().get();
+        noteRepository.saveNote(newNote);
+
+        return redirect( "/");
     }
 
 }
