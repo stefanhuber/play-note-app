a084718417977edd19d4e68752a7660bbeee98dc
Stefan Huber <stefan.huber@beyondit.at>
2018-03-28 17:45:25 +0200

    chore: update

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,19 +4,19 @@ import models.Note;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.*;
-import services.InMemoryNoteRepository;
+import services.EbeanNoteRepository;
 
 import javax.inject.Inject;
 import java.util.List;
 
 public class HomeController extends Controller {
 
-    protected InMemoryNoteRepository noteRepository;
+    protected EbeanNoteRepository noteRepository;
 
     protected Form<Note> noteForm;
 
     @Inject
-    public HomeController(InMemoryNoteRepository noteRepository, FormFactory formFactory) {
+    public HomeController(EbeanNoteRepository noteRepository, FormFactory formFactory) {
         this.noteRepository = noteRepository;
         this.noteForm = formFactory.form(Note.class);
     }
--- build.sbt
+++ build.sbt
@@ -3,8 +3,11 @@ organization := "fhku"
 
 version := "1.0-SNAPSHOT"
 
-lazy val root = (project in file(".")).enablePlugins(PlayJava)
+lazy val root = (project in file(".")).enablePlugins(PlayJava, PlayEbean)
 
 scalaVersion := "2.12.4"
 
 libraryDependencies += guice
+libraryDependencies += javaJdbc
+libraryDependencies += javaJpa
+libraryDependencies += "com.h2database" % "h2" % "1.4.197"
