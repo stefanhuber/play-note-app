a7943493c1239cdb96873c6117c82fc22384c86b
Stefan Huber <stefan.huber@beyondit.at>
2018-04-10 14:20:19 +0200

    chore: ü1 + ü2

---
+++ app/middlewares/BasicAuthenticationMiddleware.java
@@ -0,0 +1,45 @@
+package middlewares;
+
+import models.User;
+import play.Logger;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+import services.EbeanUserRepository;
+
+import javax.inject.Inject;
+import java.util.Base64;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class BasicAuthenticationMiddleware extends Action.Simple {
+
+    @Inject
+    protected EbeanUserRepository userRepository;
+
+    @Override
+    public CompletionStage<Result> call(Http.Context ctx) {
+        Optional<String> header = ctx.request().header("Authorization");
+
+        if (header.isPresent() && header.get().startsWith("Basic ")) {
+            String auth = header.get().substring(6);
+            auth = new String(Base64.getDecoder().decode(auth));
+
+            String[] credentials = auth.split(":");
+
+            if (credentials.length == 2) {
+                User user = userRepository.getUserByUsername(credentials[0]);
+                if (user != null && user.comparePasswords(credentials[1])) {
+                    return delegate.call(ctx);
+                }
+            }
+        }
+
+        Result result = unauthorized("Authentication required")
+                .withHeader("WWW-Authenticate", "Basic realm=\"Secure Area\"");
+
+        return CompletableFuture.completedFuture(result);
+
+    }
+}
---
+++ app/services/EbeanUserRepository.java
@@ -0,0 +1,29 @@
+package services;
+
+import io.ebean.Ebean;
+import models.User;
+
+public class EbeanUserRepository {
+
+    public EbeanUserRepository() {
+        if (Ebean.find(User.class).findCount() <= 0) {
+            User user1 = new User();
+            user1.setUsername("zwerg1");
+            user1.setPasswordInCleartext("festung");
+            Ebean.save(user1);
+
+            User user2 = new User();
+            user2.setUsername("zwerg2");
+            user2.setPasswordInCleartext("festung");
+            Ebean.save(user2);
+        }
+    }
+
+    public User getUserByUsername(String username) {
+        return Ebean.find(User.class)
+                .where()
+                .eq("username", username)
+                .findOne();
+    }
+
+}
